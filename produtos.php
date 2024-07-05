742fc0685c0b78ea88b69f7e9319c49ec2c06ebf
DENIS DE FERRAZ <denis.black65@gmail.com>
2024-07-05 17:15:42 -0300

    ajustes internas

--- produtos.php
+++ produtos.php
@@ -105,9 +105,13 @@ get_header(); ?>
 			.bg-bee-grey-left {
 				position: absolute;
 				height: 0;
-				left: -200px;
-				top: 0;
+				left: -100px;
+				bottom: 350px;
 				opacity: 0.1;
+				-moz-transform: scaleX(-1);
+				-o-transform: scaleX(-1);
+				-webkit-transform: scaleX(-1);
+				transform: scaleX(-1);
 			}
 
 			.bg-bee-grey-right {
@@ -116,10 +120,6 @@ get_header(); ?>
 				right: -100px;
 				bottom: 350px;
 				opacity: 0.1;
-				-moz-transform: scaleX(-1);
-				-o-transform: scaleX(-1);
-				-webkit-transform: scaleX(-1);
-				transform: scaleX(-1);
 			}
 
 			@media screen and (max-width: 768px) {
@@ -146,7 +146,7 @@ get_header(); ?>
 
 		<section class="animate__animated animate__fadeInDown animate__delay-7s" style="animation-duration: 0.5s !important">
 			<h1 class="text-center laranja-global mb-5"><?= get_the_title() ?></h1>
-			<div class="container position-relative m-4">
+			<div class="container position-relative mb-5">
 				<div class="row">
 					<div class="col-12 contato-b text-center rounded-5 p-3 m-0">
 						<h3 class="m-0">CABOS DE REDE</h3>
@@ -162,14 +162,14 @@ get_header(); ?>
 							<img class="w-100" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/' ?>empresas/dipro.png" alt="" srcset="">
 						</div>
 					</div>
-					<div class="text-cont m-4">
+					<div class="text-cont mb-5">
 						<div class="row justify-content-center">
-							<div class="col-md-10 col-12 m-4">
+							<div class="col-md-10 col-12 mb-5">
 								<p class="m-3 text-center">A Cobreflex é uma empresa brasileira com mais de 10 anos de experiência na fabricação de fios e cabos elétricos de alta qualidade, atendendo diretamente aos principais setores. produtos utilizados na geração e transmissão de energia, garantindo segurança e eficiência. </p>
 							</div>
 						</div>
 					</div>
-					<div class="prod-cont m-4">
+					<div class="prod-cont mb-5">
 						<div class="row">
 							<div class="col-md-4 col-12">
 								<div class="bg-white h-100 d-flex flex-column text-center p-2 rounded-3">
@@ -199,6 +199,12 @@ get_header(); ?>
 					</div>
 				</div>
 			</div>
+			<div class="bg-bee-grey-left">
+					<img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/' ?>bg-bee-removebg-preview.png" alt="" srcset="">
+				</div>
+				<div class="bg-bee-grey-right">
+					<img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/' ?>bg-bee-removebg-preview.png" alt="" srcset="">
+				</div>
 		</section>
 
 		<style>
